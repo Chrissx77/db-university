350cd5360a9b1e92a1e014bdf12ccdbb9d31e46f
ChrisGarozzo <garozzochristian@gmail.com>
2024-02-01 17:08:05 +0100

    added es group by & Join

---
+++ db-uni-2.sql
@@ -0,0 +1,96 @@
+-- ## Repo
+-- `db-university`
+
+-- ## DB
+-- [[Database/2024-01-31 - DB University|DB University]]
+
+-- ## Todo
+-- Dopo aver testato le vostre query con `phpMyAdmin`, riportatele in un file `txt` o `md` e caricatelo nella vostra repo.
+
+-- ### Query
+-- #### Group by
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+-- ```sql
+SELECT YEAR(students.enrolment_date) as anno, COUNT(*) as num_iscritti_anno
+FROM students
+GROUP BY YEAR(students.enrolment_date);
+
+-- ```
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+-- ```sql
+SELECT office_address,  COUNT(*) as num_teach
+FROM teachers
+GROUP BY office_address;
+
+-- ```
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame (dell'esame vogliamo solo l'`id`)
+-- ```sql
+SELECT FLOOR(AVG(vote)) as media_voto
+FROM exam_student
+GROUP BY exam_id;
+
+-- ```
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+-- ```sql
+SELECT COUNT(*) as num_cors
+FROM degrees
+GROUP BY department_id;
+
+-- ```
+
+
+-- #### Join
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+-- ```sql
+SELECT students.id as id, students.name as name, degrees.name as corsi
+FROM degrees JOIN students ON degrees.id = students.degree_id
+WHERE degrees.name LIKE 'Corso di Laurea in Economia';
+
+-- ```
+
+-- 2. Selezionare tutti i Corsi di Laurea Magistrale del Dipartimento di Neuroscienze
+-- ```sql
+SELECT*
+FROM degrees JOIN departments ON degrees.department_id = departments.id
+WHERE level LIKE 'magistrale'
+    AND departments.name LIKE 'Dipartimento di Neuroscienze';
+
+-- ```
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- ```sql
+SELECT teachers.name, courses.name
+FROM teachers, courses, course_teacher
+WHERE teachers.id = 44
+    AND teachers.id = course_teacher.teacher_id
+    AND course_teacher.course_id = courses.id;
+-- ```
+
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+-- ```sql
+SELECT students.name, students.surname, degrees.*
+FROM students,degrees
+WHERE students.degree_id = degrees.id
+ORDER BY students.surname;
+-- ```
+
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- ```sql
+
+
+-- ```
+
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
+-- ```sql
+
+-- ```
+
+
+-- ##### Bonus
+-- 7. Selezionare per ogni studente il numero di tentativi sostenuti per ogni esame, stampando anche il voto massimo. Successivamente, filtrare i tentativi con voto minimo 18.
+-- ```sql
+
+-- ```
